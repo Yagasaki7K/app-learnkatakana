15c6c5de2fabb9d02b50a1f7a82091bdce682e99
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-07 17:39:34 -0300

    Fix error to find Katakana

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,52 @@ import { useEffect, useState } from 'react'
 function App() {
 
 	const Katakana = [
-		{ romanji: 'a', katakana: 'ア' },
-		{ romanji: 'i', katakana: 'イ' },
-		{ romanji: 'u', katakana: 'ウ' },
-		{ romanji: 'e', katakana: 'エ' },
-		{ romanji: 'o', katakana: 'オ' },
-		{ romanji: 'ka', katakana: 'カ' },
-		{ romanji: 'ki', katakana: 'キ' },
-		{ romanji: 'ku', katakana: 'ク' },
-		{ romanji: 'ke', katakana: 'ケ' },
-		{ romanji: 'ko', katakana: 'コ' },
-		{ romanji: 'sa', katakana: 'サ' },
-		{ romanji: 'shi', katakana: 'シ' },
-		{ romanji: 'su', katakana: 'ス' },
-		{ romanji: 'se', katakana: 'セ' },
-		{ romanji: 'so', katakana: 'ソ' },
-		{ romanji: 'ta', katakana: 'タ' },
-		{ romanji: 'chi', katakana: 'チ' },
-		{ romanji: 'tsu', katakana: 'ツ' },
-		{ romanji: 'te', katakana: 'テ' },
-		{ romanji: 'to', katakana: 'ト' },
-		{ romanji: 'na', katakana: 'ナ' },
-		{ romanji: 'ni', katakana: 'ニ' },
-		{ romanji: 'nu', katakana: 'ヌ' },
-		{ romanji: 'ne', katakana: 'ネ' },
-		{ romanji: 'no', katakana: 'ノ' },
-		{ romanji: 'ha', katakana: 'ハ' },
-		{ romanji: 'hi', katakana: 'ヒ' },
-		{ romanji: 'fu', katakana: 'フ' },
-		{ romanji: 'he', katakana: 'ヘ' },
-		{ romanji: 'ho', katakana: 'ホ' },
-		{ romanji: 'ma', katakana: 'マ' },
-		{ romanji: 'mi', katakana: 'ミ' },
-		{ romanji: 'mu', katakana: 'ム' },
-		{ romanji: 'me', katakana: 'メ' },
-		{ romanji: 'mo', katakana: 'モ' },
-		{ romanji: 'ya', katakana: 'ヤ' },
-		{ romanji: 'yu', katakana: 'ユ' },
-		{ romanji: 'yo', katakana: 'ヨ' },
-		{ romanji: 'ra', katakana: 'ラ' },
-		{ romanji: 'ri', katakana: 'リ' },
-		{ romanji: 'ru', katakana: 'ル' },
-		{ romanji: 're', katakana: 'レ' },
-		{ romanji: 'ro', katakana: 'ロ' },
-		{ romanji: 'wa', katakana: 'ワ' },
-		{ romanji: 'wo', katakana: 'ヲ' },
-		{ romanji: 'n', katakana: 'ン' }
+		{ romanji: 'a', Katakana: 'ア' },
+		{ romanji: 'i', Katakana: 'イ' },
+		{ romanji: 'u', Katakana: 'ウ' },
+		{ romanji: 'e', Katakana: 'エ' },
+		{ romanji: 'o', Katakana: 'オ' },
+		{ romanji: 'ka', Katakana: 'カ' },
+		{ romanji: 'ki', Katakana: 'キ' },
+		{ romanji: 'ku', Katakana: 'ク' },
+		{ romanji: 'ke', Katakana: 'ケ' },
+		{ romanji: 'ko', Katakana: 'コ' },
+		{ romanji: 'sa', Katakana: 'サ' },
+		{ romanji: 'shi', Katakana: 'シ' },
+		{ romanji: 'su', Katakana: 'ス' },
+		{ romanji: 'se', Katakana: 'セ' },
+		{ romanji: 'so', Katakana: 'ソ' },
+		{ romanji: 'ta', Katakana: 'タ' },
+		{ romanji: 'chi', Katakana: 'チ' },
+		{ romanji: 'tsu', Katakana: 'ツ' },
+		{ romanji: 'te', Katakana: 'テ' },
+		{ romanji: 'to', Katakana: 'ト' },
+		{ romanji: 'na', Katakana: 'ナ' },
+		{ romanji: 'ni', Katakana: 'ニ' },
+		{ romanji: 'nu', Katakana: 'ヌ' },
+		{ romanji: 'ne', Katakana: 'ネ' },
+		{ romanji: 'no', Katakana: 'ノ' },
+		{ romanji: 'ha', Katakana: 'ハ' },
+		{ romanji: 'hi', Katakana: 'ヒ' },
+		{ romanji: 'fu', Katakana: 'フ' },
+		{ romanji: 'he', Katakana: 'ヘ' },
+		{ romanji: 'ho', Katakana: 'ホ' },
+		{ romanji: 'ma', Katakana: 'マ' },
+		{ romanji: 'mi', Katakana: 'ミ' },
+		{ romanji: 'mu', Katakana: 'ム' },
+		{ romanji: 'me', Katakana: 'メ' },
+		{ romanji: 'mo', Katakana: 'モ' },
+		{ romanji: 'ya', Katakana: 'ヤ' },
+		{ romanji: 'yu', Katakana: 'ユ' },
+		{ romanji: 'yo', Katakana: 'ヨ' },
+		{ romanji: 'ra', Katakana: 'ラ' },
+		{ romanji: 'ri', Katakana: 'リ' },
+		{ romanji: 'ru', Katakana: 'ル' },
+		{ romanji: 're', Katakana: 'レ' },
+		{ romanji: 'ro', Katakana: 'ロ' },
+		{ romanji: 'wa', Katakana: 'ワ' },
+		{ romanji: 'wo', Katakana: 'ヲ' },
+		{ romanji: 'n', Katakana: 'ン' }
 	]
 
 	const [input, setInput] = useState('')
@@ -60,7 +60,7 @@ function App() {
 	const [error, setError] = useState(false)
 
 	const setRandomKatakana = () => {
-		const randomIndex = Math.floor(Math.random() * katakana.length)
+		const randomIndex = Math.floor(Math.random() * Katakana.length)
 		setCurrent(randomIndex)
 	}
 
@@ -71,7 +71,7 @@ function App() {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		 
-		if (input.toLowerCase() === katakana[current].romanji) {
+		if (input.toLowerCase() === Katakana[current].romanji) {
 			setStreak(streak + 1)
 			setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak)
 			setError(false)
@@ -79,8 +79,8 @@ function App() {
 			localStorage.setItem('streak', streak + 1)
 			localStorage.setItem('maxStreak', streak + 1 > maxStreak ? streak + 1 : maxStreak)
 		} else {
-			const h = katakana[current].katakana
-			const r = katakana[current].romanji
+			const h = Katakana[current].Katakana
+			const r = Katakana[current].romanji
 			setError(`Wrong! The correct answer for ${h} is ${r}`)
 			setStreak(0)
 			localStorage.setItem('streak', 0)
@@ -106,7 +106,7 @@ function App() {
 			</header>
 
 			<div className="text-9xl font-bold mb-8">
-				<p>{katakana[current].katakana}</p>
+				<p>{Katakana[current].Katakana}</p>
 			</div>
 
 			<div className="mb-8">
